54c8d417948677429bb0b282df900f41d31f0a3a
Karthik <karthik@success>
2022-04-21 14:55:49 +0530

    updated function for voting_age

--- basics/voting_age.php
+++ basics/voting_age.php
@@ -1,9 +1,15 @@
-<?php 
-$age=18;
-if ($age <=17){
-    echo "Not eligible for voting";
-} else{
-    echo "Eligible for voting";
+<?php declare(strict_types=1);
+function Voting_age(int $age)
+{
+    if ($age <=17){
+        echo "Not Eligibe for voting";
+    }
+    else{
+        echo "Eligible for voting";
+    }
+
 }
+
+Voting_age(19)
 
 ?>
