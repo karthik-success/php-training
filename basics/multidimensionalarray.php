a4509ae55546b0d81efbe00ff9b9a344fdb02f41
Karthik <karthik@success>
2022-04-27 17:31:00 +0530

    changes in code

--- basics/multidimensionalarray.php
+++ basics/multidimensionalarray.php
@@ -1,38 +1,66 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<body>
 <?php
-$resume=[
+$resumes=[
     [
-        "name"=>"karthik ",
-        "age"=>'22 ',
-        "skill"=>['python','machinelearning','tableau'],
+        "name"=>"karthik",
+        "age"=>22,
         "emailid"=>"karthik@gmail.com ",
-        "job role"=>"Data analysist ",
-        "phone no"=>6383858698
-    ],
-    [
-        "name"=>"cheran ",
-        "age"=>'28 ' ,
-        "skill"=>['php','laravel','mysql'],
-        "emailid"=>"cheran@gmail.com ",
-        "job role"=>"fullstack developer ",
-        "phone no"=>6383855671
-
-    ],
-    [
-        "name"=>"jithesh ",
-        "age"=>'24 ' ,
-        "skill"=>['javascript','css','html'],
-        "emailid"=>"cheran@gmail.com ",
-        "job role"=>"backend developer ",
-        "phone no"=>6383855645 
+        "phoneNo"=>638385879,
+        "about_me"=>"I am an Aspiring Engineer who loves to code",
+        "skills"=>['php','laravel','mysql'],
+        "email_id"=>"cheran@gmail.com ",
+        "job_role"=>"Data analysist ",
+        "education"=>"BCA", 
+        "college_name"=>"Jain_College",
+        "school_name"=>"Alagappa",
+        "school_percentage"=>"90%",
+        "College_percentage"=>"70%",
+        "hobby"=>"Book reading",
+        "sports"=>['cricket','volleyball','Shuttle'],
+        "qualities"=>['Leadership','Determination','Loyalty'],
+        "address"=>[
+            "current_address"=>[
+                "doornumber"=>1,
+                "street_name"=>"Purasawalkam",
+                "city"=>"Chennai",
+                "pincode"=>600023
+        
+        ],
+        "permanent_address"=>[
+            "doornumber"=>2,
+            "street_name"=>"aynavaram",
+            "city"=>"Chennai",
+            "pincode"=>600030
+    ]         
 
     ]
+           
+    ],
+    "salary"=>[
+        "current_salary"=>[
+            "salary_range"=>"3LPA-5LPA"
+    ],
+    "expected_salary"=>[
+        "salary_range"=>"7LPA-10LPA"
+    ]
+    ]
 
-];
-//echo $resume[0]["skill"][1];
-foreach($resume as $res){
-     //var_dump ($resume);
-    echo $res['age'];
+    
+    ];
+
+foreach($resumes as $resume){
+    //echo "Personal information is <br>";
+    var_dump ($resume) ;
+    //echo $resume["age"];
+    //echo $resume["address"];
 }
 
+
+
 ?>
+</body>
+</html>
+
