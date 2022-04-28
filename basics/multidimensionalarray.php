8d8387c7409460e84ffdf88556813ab2a290a417
Karthik <karthik@success>
2022-04-28 10:54:14 +0530

    added another resume

--- basics/multidimensionalarray.php
+++ basics/multidimensionalarray.php
@@ -10,8 +10,8 @@ $resumes=[
         "emailid"=>"karthik@gmail.com ",
         "phoneNo"=>638385879,
         "about_me"=>"I am an Aspiring Engineer who loves to code",
-        "skills"=>['php','laravel','mysql'],
-        "email_id"=>"cheran@gmail.com ",
+        "skills"=>['tableau','machinelearning','mysql'],
+        "backup_email_id"=>"cheran@gmail.com ",
         "job_role"=>"Data analysist ",
         "education"=>"BCA", 
         "college_name"=>"Jain_College",
@@ -45,6 +45,49 @@ $resumes=[
     ],
     "expected_salary"=>[
         "salary_range"=>"7LPA-10LPA"
+    ]
+    ],
+    [
+        "name"=>"cheran",
+        "age"=>28,
+        "emailid"=>"cheran@gmail.com ",
+        "phoneNo"=>6383854569,
+        "about_me"=>"I am an Aspiring Engineer who loves to code and I am senior developer",
+        "skills"=>['php','laravel','mysql','mongodb','css'],
+        "backup_email_id"=>"cheran@gmail.com ",
+        "job_role"=>"Senior developer ",
+        "education"=>"mechanical engineer", 
+        "college_name"=>"savitha_College",
+        "school_name"=>"satya",
+        "school_percentage"=>"80%",
+        "College_percentage"=>"750%",
+        "hobby"=>"Book reading",
+        "sports"=>['cricket','volleyball','Shuttle','basketball'],
+        "qualities"=>['Leadership','Determination','Loyalty','good performer'],
+        "address"=>[
+            "current_address"=>[
+                "doornumber"=>2,
+                "street_name"=>"tnagar",
+                "city"=>"Chennai",
+                "pincode"=>600027
+        
+        ],
+        "permanent_address"=>[
+            "doornumber"=>5,
+            "street_name"=>"kodambakam",
+            "city"=>"Chennai",
+            "pincode"=>600035
+    ]         
+
+    ]
+           
+    ],
+    "salary"=>[
+        "current_salary"=>[
+            "salary_range"=>"5LPA-7LPA"
+    ],
+    "expected_salary"=>[
+        "salary_range"=>"9LPA-11LPA"
     ]
     ]
 
