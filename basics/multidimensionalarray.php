094f2b9f961c8c662443c82263bfadfea1f26390
Karthik <karthik@success>
2022-04-29 10:56:49 +0530

    Updated two more resumes

--- basics/multidimensionalarray.php
+++ basics/multidimensionalarray.php
@@ -49,9 +49,105 @@ $resumes=[
 
         ]  
     ],
+    [
+        "name"=>"Cheran",
+        "age"=>28,
+        "emailid"=>"cheran@gmail.com ",
+        "phone_no"=>9840270450,
+        "about_me"=>"I am a Senior developer who train developers",
+        "skills"=>['php','laravel','mysql'],
+        "backup_email_id"=>"cherannaruto@gmail.com ",
+        "job_role"=>"Senior Developer ",
+        "education"=>"Mechanical Engineer", 
+        "college_name"=>"Vaishnava_College",
+        "school_name"=>"Anitha",
+        "school_percentage"=>"80%",
+        "college_percentage"=>"75%",
+        "hobby"=>["Book reading","watching series"],
+        "sports"=>['basketball','golf','Shuttle'],
+        "qualities"=>['Leadership','Determination'],
+        "address"=>[
+            "current_address"=>[
+                "doornumber"=>10,
+                "street_name"=>"kodambakam",
+                "city"=>"Chennai",
+                "pincode"=>600040
+        
+            ],
+            "permanent_address"=>[
+                "doornumber"=>20,
+                "street_name"=>"Nungabakam",
+                "city"=>"Chennai",
+                "pincode"=>600045
+            ]         
+
+        ],
+        "salary"=>[
+            "current_salary"=>[
+                "salary_range"=>"7LPA-9LPA"
+
+            ],
+            "expected_salary"=>[
+                "salary_range"=>"11LPA-12LPA"
+            ]
+
+
+        ]
+
+
+    ],
+    [
+        "name"=>"Jithesh",
+        "age"=>24,
+        "emailid"=>"jithesh@gmail.com ",
+        "phone_no"=>8438489524,
+        "about_me"=>"I am a Junior developer who loves to code",
+        "skills"=>['html','java_script','mysql'],
+        "backup_email_id"=>"jitheshsasuke@gmail.com ",
+        "job_role"=>"Junior Developer ",
+        "education"=>"Computer Engineer", 
+        "college_name"=>"Veltech_College",
+        "school_name"=>"Seventhday",
+        "school_percentage"=>"79%",
+        "college_percentage"=>"81%",
+        "hobby"=>["Trecking","Gymming"],
+        "sports"=>['football','throwball','Shuttle'],
+        "qualities"=>['Aspiring','Determination'],
+        "address"=>[
+            "current_address"=>[
+                "doornumber"=>21,
+                "street_name"=>"tnagar",
+                "city"=>"Chennai",
+                "pincode"=>600029
+        
+            ],
+            "permanent_address"=>[
+                "doornumber"=>69,
+                "street_name"=>"pallavaram",
+                "city"=>"Chennai",
+                "pincode"=>600073
+            ]         
+
+        ],
+        "salary"=>[
+            "current_salary"=>[
+                "salary_range"=>"2LPA-4LPA"
+
+            ],
+            "expected_salary"=>[
+                "salary_range"=>"5LPA-7LPA"
+            ]
+
+
+        ]
+
+
+
+    ],
+
     
     
-    ];
+];
 //echo $resume[0][0];
 foreach($resumes as $resume){
     echo "Name =>".$resume['name']."\n"."<br>";
@@ -89,6 +185,7 @@ foreach($resumes as $resume){
         echo "Salary=>".$salary["salary_range"]."\n"."<br>";
       
     }
+    echo "<br>";
 
 
 
